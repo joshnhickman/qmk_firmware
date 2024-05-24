70de2a407462f5e6d1ff02425c326fb0fd605f40
joshnhickman <joshnhickman@gmail.com>
2024-05-24 12:35:02 -0700

    updates

--- keyboards/planck/keymaps/joshnhickman/keymap.c
+++ keyboards/planck/keymaps/joshnhickman/keymap.c
@@ -43,14 +43,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | GUI  | Alt  |Lower | Space|Space |Raise | Left | Down |  Up  |Right |
+ * |      | Ctrl | GUI  | Alt  |Lower | Space|Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    _______, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -59,16 +59,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      |      |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |   [  |   ]  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      | Space|Space |      | Home |Pg Dn |Pg Up | End  |
+ * |Enter |      |      |      |------|      |      |      | Home |Pg Dn |Pg Up | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,    _______,    _______, _______,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_HOME,    KC_PGDN, KC_PGUP, KC_END
+    KC_ENT,  _______, _______, _______, _______, _______, _______, _______,    KC_HOME,    KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Raise
@@ -79,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
+ * |      |      |      |      |      |      |------|      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------',
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
@@ -90,22 +90,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 /* Adjust (Lower + Raise)
- *                      v------------------------RGB CONTROL--------------------v
  * ,-----------------------------------------------------------------------------------.
- * |PAUSE | BOOT |Debug |      |      |      |      |      |      |      |      |  Del |
+ * |PAUSE | BOOT |Debug |      |      |      |      |      |INSERT|      |      |  Del |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |MUStog|      |MOUSE1|MOUSE2|      |      |MOUSE<|MOUSEv|MOUSE^|MOUSE>|      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |AUDtog|      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      | HUE- |RGBtog|BRGHT+| SAT+ |
+ * |      |      |      |      |------|      |------|      | HUE- |RGBtog|BRGHT+| SAT+ |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    KC_PAUSE, QK_BOOT, DB_TOGG,    _______,    _______, _______, _______,    _______,    _______,  _______,     _______,  KC_DEL,
+    KC_PAUSE, QK_BOOT, DB_TOGG,    _______,    _______, _______, _______,    _______,  KC_INSERT,  _______,     _______,  KC_DEL,
      _______, MU_TOGG, _______, KC_MS_BTN1, KC_MS_BTN2, _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, _______,
      _______, AU_TOGG, _______,    _______,    _______, _______, _______,    _______,    _______,  _______,     _______, _______,
-     _______, _______, _______,    _______,    _______, _______, _______,    _______,    _______,  _______,     _______, _______
+     _______, _______, _______,    _______,    _______, _______, _______,    _______,    RGB_HUD,  RGB_TOG,     RGB_VAI, RGB_SAI
 )
 
 };
